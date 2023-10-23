abc5be5a95c920b13129b026199eb8f3881cc0ba
Ignacio Gallego Sagastume <ignacio.gallegosagastume@hivemindtechnologies.com>
2023-10-23 10:24:45 +0200

    Write some more tests

--- src/test/scala/com/hivemind/app/database/DatabaseSpec.scala
+++ src/test/scala/com/hivemind/app/database/DatabaseSpec.scala
@@ -9,7 +9,6 @@ import zio.test.*
 import zio.test.Assertion.*
 
 import java.io.IOException
-import scala.reflect.ClassTag
 
 object DatabaseSpec extends ZIOSpecDefault {
 
@@ -20,7 +19,7 @@ object DatabaseSpec extends ZIOSpecDefault {
     } yield assertTrue(output == Vector("Hello, World!\n"))
   }
 
-  val test1: Spec[Any, DatabaseException] = test("returns data when getObjectById(1, TableName.Users) is executed") {
+  val test1: Spec[Any, DatabaseException] = test("returns Alonzo Church when getObjectById is executed") {
     val fixture = new TestConfiguration
 
     for {
@@ -31,7 +30,7 @@ object DatabaseSpec extends ZIOSpecDefault {
 
   val test2: Spec[Any, Option[Record]] = test("returns an exception when getObjectById is executed") {
     val fixture2 = new TestConfiguration {
-      override lazy val probability: Double = 100.0
+      override lazy val probabilityOfErrors: Double = 100.0
     }
 
     for {
@@ -40,15 +39,51 @@ object DatabaseSpec extends ZIOSpecDefault {
     } yield assert(error)(isSubtype[DatabaseConnectionClosedException](anything))
   }
 
-  def spec: Spec[TestEnvironment with Scope, Any] = suite("Database implementation")(helloWorldTest, test1, test2)
+  val test3: Spec[Any, DatabaseException] = test("returns Alan Turing when getObjectById is executed") {
+    val fixture = new TestConfiguration
+
+    for {
+      db     <- fixture.databaseIO
+      record <- db.getObjectById(2, TableName.Users)
+    } yield assertAlanTuring(record)
+  }
+
+  val test4: Spec[Any, DatabaseException] = test("returns property 1 when getObjectById is executed") {
+    val fixture = new TestConfiguration
+
+    for {
+      db     <- fixture.databaseIO
+      record <- db.getObjectById(1, TableName.Properties)
+    } yield assertProperty1(record)
+  }
+
+  val test5: Spec[Any, DatabaseException] = test("returns property 2 when getObjectById is executed") {
+    val fixture = new TestConfiguration
+
+    for {
+      db     <- fixture.databaseIO
+      record <- db.getObjectById(2, TableName.Properties)
+    } yield assertProperty2(record)
+  }
+
+  def spec: Spec[TestEnvironment with Scope, Any] = suite("Database implementation")(helloWorldTest, test1, test2, test3, test4, test5)
 
   private def assertAlonzoChurch(value: Option[Record]): TestResult =
     assert(value)(isSome(equalTo(UserRecord(id = 1, name = "Alonzo", surname = "Church", age = 33))))
+
+  private def assertAlanTuring(value: Option[Record]): TestResult =
+    assert(value)(isSome(equalTo(UserRecord(id = 2, name = "Alan", surname = "Turing", age = 31))))
+
+  private def assertProperty1(value: Option[Record]): TestResult =
+    assert(value)(isSome(equalTo(PropertyRecord(id = 1, propertyType = "Car", price = 17800, owner = 1))))
+
+  private def assertProperty2(value: Option[Record]): TestResult =
+    assert(value)(isSome(equalTo(PropertyRecord(id = 2, propertyType = "House", price = 230500, owner = 1))))
 }
 
 class TestConfiguration {
-  lazy val probability: Double                        = neverFails
-  lazy val testConfig: Config                         = Config(DatabaseParameters("myDB", "password", 5, probabilityOfError = probability))
+  lazy val probabilityOfErrors: Double                = neverFails
+  lazy val testConfig: Config                         = Config(DatabaseParameters("myDB", "password", 5, probabilityOfError = probabilityOfErrors))
   val neverFails: Double                              = 0.0
   val alwaysFail: Double                              = 100.0
   val testConfigZLayer: ULayer[Config]                = ZLayer.succeed(testConfig)
