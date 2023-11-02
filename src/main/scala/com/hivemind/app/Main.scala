6e6666334fd635f9e137761d26d2e2f5e1b9e93b
Ignacio Gallego Sagastume <ignacio.gallegosagastume@hivemindtechnologies.com>
2023-11-02 09:31:10 +0100

    Format main and fix typo

--- src/main/scala/com/hivemind/app/Main.scala
+++ src/main/scala/com/hivemind/app/Main.scala
@@ -15,27 +15,27 @@ object Main extends ZIOAppDefault {
 
   private val myAppLogic: ZIO[Logger with UserService with PropertyService, IOException, Unit] =
     for {
-      logging     <- ZIO.service[Logger]
-      userService <- ZIO.service[UserService]
+      logging         <- ZIO.service[Logger]
+      userService     <- ZIO.service[UserService]
       propertyService <- ZIO.service[PropertyService]
-      _      <- logging.log("Starting application ...", HivemindLogLevel.INFO)
-      _      <- logging.log("Looking for user with id=1")
-      alonzo <- userService
-                  .findUser(1)
-                  .mapError(err => IOException(s"UserService failed to find user with id 1", err))
-                  .flatMap(ZIO.fromOption)
-                  .orElseFail(IOException(s"Did not find user with Iid 1"))
-      _      <- logging.log(s"Found user '${alonzo.name} ${alonzo.surname}'")
-      _      <- logging.log(s"Looking for ${alonzo.name}'s properties")
-      properties <- propertyService
-        .findPropertiesOfUser(alonzo.id)
-        .mapError(err => IOException(s"PropertyService failed to find properties for user with id ${alonzo.id}", err))
-      propertiesAsStr = properties.map(property => s"{ id: ${property.id}, kind: ${property.kind}, price: €${property.price} }").mkString("\n")
-      _ <- logging.log(s"User ${alonzo.name} has the following properties:\n$propertiesAsStr")
-      _ <- logging.log("Ending application")
+      _               <- logging.log("Starting application ...", HivemindLogLevel.INFO)
+      _               <- logging.log("Looking for user with id=1")
+      alonzo          <- userService
+                           .findUser(1)
+                           .mapError(err => IOException(s"UserService failed to find user with id 1", err))
+                           .flatMap(ZIO.fromOption)
+                           .orElseFail(IOException(s"Did not find user with id 1"))
+      _               <- logging.log(s"Found user '${alonzo.name} ${alonzo.surname}'")
+      _               <- logging.log(s"Looking for ${alonzo.name}'s properties")
+      properties      <- propertyService
+                           .findPropertiesOfUser(alonzo.id)
+                           .mapError(err => IOException(s"PropertyService failed to find properties for user with id ${alonzo.id}", err))
+      propertiesAsStr  = properties.map(property => s"{ id: ${property.id}, kind: ${property.kind}, price: €${property.price} }").mkString("\n")
+      _               <- logging.log(s"User ${alonzo.name} has the following properties:\n$propertiesAsStr")
+      _               <- logging.log("Ending application")
     } yield ()
 
-  def run: ZIO[Any, IOException, Unit] =
+  override def run: ZIO[Any, IOException, Unit] =
     myAppLogic.provide(
       Logger.live,
       ZLayer.succeed(zio.Console.ConsoleLive),
